24026f5ab607907b39604760b10e48f1c0d9ad1c
ericlian <ericlian419@gmail.com>
2020-09-19 01:06:44 -0400

    update package

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataGetService } from '../shared/data-get.service';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,17 @@ import { DataGetService } from '../shared/data-get.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  private map;
   constructor(public dataGetService: DataGetService) { }
 
+	private initMap(): void {
+	  this.map = L.map('map', {
+	    center: [ 39.8282, -98.5795 ],
+	    zoom: 3
+	  });
+	}
   ngOnInit() {
     this.dataGetService.getCategories();
+
   }
 }
